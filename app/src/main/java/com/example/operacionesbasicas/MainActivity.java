e42b5bd4e982a22164cb287a304ad8d5770bf223
K1050155850 <lemastalin344@gmail.com>
2023-05-22 18:04:57 -0500

    Parte 2 con programacion

--- app/src/main/java/com/example/operacionesbasicas/MainActivity.java
+++ app/src/main/java/com/example/operacionesbasicas/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.operacionesbasicas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void irSuma(View view){
+        Intent intent= new Intent(this, sumaActivity.class);
+        view.getContext().startActivity(intent);
+    }
+    //VIEW es la super clase de vista grafica
+    //INTENT es un objeto que indica la actividad que va a iniciar
+    //START ACTIVITY inicia el ciclo de vida de nuestra actividad
 }
--- app/src/main/java/com/example/operacionesbasicas/sumaActivity.java
+++ app/src/main/java/com/example/operacionesbasicas/sumaActivity.java
@@ -3,6 +3,9 @@ package com.example.operacionesbasicas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class sumaActivity extends AppCompatActivity {
 
@@ -11,4 +14,22 @@ public class sumaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suma);
     }
+    public void regresaMenu(View view){
+        this.finish();
+    }
+
+    public void Sumar(View view){
+        //Accedemos al ID que colocamos en los botones y text fields
+        EditText numero1 = findViewById(R.id.txt_num1);
+        EditText numero2 = findViewById(R.id.txt_num2);
+        TextView total = findViewById(R.id.txt_resultado);
+
+        //Operamos con las variales
+        double resultado = Double.parseDouble( numero1.getText().toString() )+ Double.parseDouble( numero2.getText().toString());
+
+        //Imprimimos los resultados
+
+        total.setText(resultado+""); //Dentro del set tex lo colocamos como cadena poniendo resultado + ""
+    }
+
 }
