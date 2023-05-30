e485c26122d833ab0d8046d7358d3130bac05a2c
K1050155850 <lemastalin344@gmail.com>
2023-05-30 00:17:36 -0500

    Funcionalidad para hablar en el microfono y que recnozca la voz

--- app/src/main/java/com/example/operacionesbasicas/sumaActivity.java
+++ app/src/main/java/com/example/operacionesbasicas/sumaActivity.java
@@ -2,12 +2,20 @@ package com.example.operacionesbasicas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class sumaActivity extends AppCompatActivity {
+
+    public static final int RECOGNIZE_SPEECH_ACTIVITY = 1 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,12 +75,43 @@ public class sumaActivity extends AppCompatActivity {
         TextView total = findViewById(R.id.txt_resultado);
 
         //Operamos con las variales
-        double resultado = Double.parseDouble( numero1.getText().toString() )+ Double.parseDouble( numero2.getText().toString());
+        double resultado = Double.parseDouble( numero1.getText().toString() )/ Double.parseDouble( numero2.getText().toString());
 
         //Imprimimos los resultados
 
         total.setText(resultado+""); //Dentro del set tex lo colocamos como cadena poniendo resultado + ""
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case RECOGNIZE_SPEECH_ACTIVITY:
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> speech = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String strSpeech2Text = speech.get(0);
+                    //grabar.setText(strSpeech2Text);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    public void onClickImgBtnHablar(View v) {
+        Intent intentActionRecognizeSpeech = new Intent(
+                RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intentActionRecognizeSpeech.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL, "es-ES");
+        try {
+            startActivityForResult(intentActionRecognizeSpeech,
+                    RECOGNIZE_SPEECH_ACTIVITY);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    "No se pudo realizar el reconocimiento de voz",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
 
 }
